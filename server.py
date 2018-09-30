0106b523f3a2ae69e65c01fde4669ad4f31f4d2f
Parin Kobboon <punparin@gmail.com>
2018-09-30 14:30:49 +0700

    Added update_request() in Flask

--- server.py
+++ server.py
@@ -6,12 +6,13 @@ from werkzeug.utils import secure_filename
 from PySide2 import QtCore
 from ISA_UI import *
 
-
+# Init URL and port
 PORT = 5000
 ROOT_URL = 'http://localhost:{}'.format(PORT)
 
 app = Flask(__name__)
 
+# Set recievable file
 UPLOAD_FOLDER = ''
 ALLOWED_EXTENSIONS = set(['wav'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -21,26 +22,31 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/test/post', methods=['POST'])
-def get_tasks():
-    if request.method == 'POST':
-        if request.method == 'POST':
-        # check if the post request has the file part
-            if 'file' not in request.files:
-                return "No file"
-            file = request.files['file']
-            # if user does not select file, browser also
-            # submit a empty part without filename
-            if file.filename == '':
-                return "without filename"
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                return "success"
+def update_request():
+    # Call the UI application in Flask
+    w = FlaskThread._single.application
+
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        return "No file"
+    file = request.files['file']
+
+    # if user does not select file, browser also
+    # submit a empty part without filename
+    if file.filename == '':
+        return "without filename"
+
+    if file and allowed_file(file.filename):
+        file_name = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file_name))
+        w.update_request(file_name)
+        return "success"
 
 
 class FlaskThread(QtCore.QThread):
-    signal = QtCore.Signal(str)
+    # Singleton Flask
     _single = None
+
     def __init__(self, application):
         QtCore.QThread.__init__(self)
         if FlaskThread._single:
@@ -52,20 +58,19 @@ class FlaskThread(QtCore.QThread):
         self.wait()
 
     def run(self):
-        self.application.run(port=PORT)
+        self.application.run(port=PORT, debug=True, use_reloader=False)
 
 def start_GUI(application):
     qtApp = QApplication(sys.argv)
 
     webapp = FlaskThread(application)
-
     w = ISA_UI()
+
+    webapp.start()
     w.show()
 
     qtApp.aboutToQuit.connect(webapp.terminate)
     QApplication.setQuitOnLastWindowClosed(False)
-
-    webapp.start()
 
     return qtApp.exec_()
 
